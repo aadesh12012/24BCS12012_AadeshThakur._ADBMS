e7fdf00662656f26700ecd64bcab5ffcc4dddd34
aadesh12012 <aadesuthakur@gmail.com>
2026-08-10 21:24:19 +0530

    Update 2.1.sql

--- Experiments/Experiment 2/2.1/2.1.sql
+++ Experiments/Experiment 2/2.1/2.1.sql
@@ -1,6 +1,6 @@
 --https://www.codechef.com/learn/course/sql-intermediate/SQ00BS02/problems/GSQ66
 
---Submission Link - https://www.codechef.com/viewsolution/1312927364
+
 
 /* Write a query using union to stack the table 'Arts' over 'Science' and output the final table */
 
