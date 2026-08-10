bd95e8c855abd54762518daba101976c116e8841
aadesh12012 <aadesuthakur@gmail.com>
2026-08-10 21:25:12 +0530

    Add query to select fruit names from inventory
    Add SQL query to retrieve available fruits from supermarket.

--- Experiments/Experiment 2/2.3/2.3.sql
+++ Experiments/Experiment 2/2.3/2.3.sql
@@ -1,6 +1,6 @@
 --https://www.codechef.com/learn/course/sql-intermediate/SQ00BS06/problems/GSQ76?tab=statement
 
---Submission Link - https://www.codechef.com/viewsolution/1312935534
+
 
 /* Write a query to find the list of fruits available in the supermarket.
 (f_name column has the name of the fruits and inv_name has the name of inventories, you are suppose to output the name of the fruits.)*/
